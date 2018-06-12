494c6eae4606df9495f15a74889530e450207182
Chris Bradley <chris@cbxiii.com>
2018-06-12 16:51:05 +0100

    Bug fix in script.js

--- views/script.js
+++ views/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
   
-  // http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=APIKEYHERE
+  // http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=c0adb0b1df35ed420e67d2c8c375fcfb
   
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=c0adb0b1df35ed420e67d2c8c375fcfb", function(weather){
+
+  	console.log(weather.main.temp);
+
+  	var temp = weather.main.temp;
+
+  	$("p.api").html("The current temperature in London is " + temp + "&deg;C");
+
+
   });
 
 });
