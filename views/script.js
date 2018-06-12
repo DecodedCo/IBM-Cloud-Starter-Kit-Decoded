f40e55812da670addede2de9dd0569f233cc9ef5
Chris Bradley <chris@cbxiii.com>
2018-06-12 14:52:21 +0100

    Finishing off the template

--- views/script.js
+++ views/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   
-  // Use getJson function to get information from above URL
-  // http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=c0adb0b1df35ed420e67d2c8c375fcfb
+  // http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=APIKEYHERE
   
   });
 
