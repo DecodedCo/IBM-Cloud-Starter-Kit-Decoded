95524ab3f58ebd4149e0dfccb38f0ff33486d6ed
Chris Bradley <chris@cbxiii.com>
2018-09-27 14:57:57 +0100

    new script based on pulling in data from sensorTag

--- views/script.js
+++ views/script.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function(){
   
   // get a valid API key for OpenWeatherMap, then replace APIKEYHERE with your actual key in the following line 
-  var openweatherURL = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=APIKEYHERE"
+  var sensorURL = "http://127.0.0.1:1880/data"
   
-  jQuery.getJSON(openweatherURL, function(data){
+  jQuery.getJSON(sensorURL, function(sensorData){
 
-  	console.log(data.main.temp);
-    var temperature = data.main.temp;
+  	console.log(sensorData);
+
     // change the following line to show the current temperature 
     jQuery("p.api").html("This paragraph is controlled by jQuery");
 
